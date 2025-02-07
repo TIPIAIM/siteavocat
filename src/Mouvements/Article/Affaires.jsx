112a318fe88c999d4f872b35f43624738ee98330
alphaousmanediallo9 <alphaousmaneousmane@gmail.com>
2025-02-07 21:59:11 +0100

    Fix routes for Vercel81

--- src/Mouvements/Article/Affaires.jsx
+++ src/Mouvements/Article/Affaires.jsx
@@ -1,39 +1,43 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
-import articleImage from "./../../assets/Image/avoc3.avif"; // Image spécifique à l'article
-import Footer from "../Accueil/Footerr";
-import Affairpart1 from "./Affairpart1";
-import Affairpart2 from "./Affairpart2";
+import React, { useEffect, Suspense } from "react"; // Importation des modules React
+import styled from "styled-components"; // Importation de styled-components pour les styles
+import "aos/dist/aos.css"; // Importation du CSS de AOS pour les animations
+import articleImage from "./../../assets/Image/avoc3.avif"; // Importation de l'image de l'article
+import tiptamcode from "./../../assets/Image/tiptamcode.avif"; // Importation de l'image de l'article
 
-// Conteneur principal avec superposition
-// Conteneur principal avec superposition
+// Importation dynamique des composants pour le chargement différé (lazy loading)
+const Footer = React.lazy(() => import("../Accueil/Footerr"));
+const Affair = React.lazy(() => import("./Affairpart2"));
+const Affairfinal = React.lazy(() => import("./Affairpart1"));
+
+// Conteneur principal avec superposition sombre
 const AffairesContainer = styled.div`
   width: 100%;
-  min-height: 100vh;
-  padding: 50px 20px;
-  position: relative;
-  background: url("img/logoAODnoir.avif") center/cover no-repeat;
-  background-attachment: fixed; /* Image d'arrière-plan fixe */
+  min-height: 100vh; /* Hauteur minimale de la vue */
+  padding: 50px 20px; /* Espacement interne */
+  position: relative; /* Position relative pour les éléments enfants */
+  background: url("img/logoAODnoir.avif") center/cover no-repeat; /* Image de fond */
+  background-attachment: fixed; /* Fond fixe lors du défilement */
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  color: white;
+  color: white; /* Couleur du texte */
   text-align: center;
 
   &:before {
     content: "";
     position: absolute;
-    inset: 0;
+    inset: 0; /* Couvre tout l'espace du conteneur */
     background: rgba(0, 0, 0, 0.7); /* Superposition sombre */
-    z-index: 1;
+    z-index: 1; /* Positionne au-dessus du fond */
   }
 `;
 
+// Wrapper pour le contenu principal
 const ContentWrapper = styled.div`
   position: relative;
-  z-index: 2;
-  max-width: 1200px;
+  z-index: 2; /* Positionne au-dessus de la superposition sombre */
+  max-width: 1200px; /* Largeur maximale du contenu */
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -42,187 +46,256 @@ const ContentWrapper = styled.div`
 
 // Titre de la section
 const SectionTitle = styled.h2`
-  font-size: 2.8rem;
-  font-weight: bold;
-  margin-bottom: 40px;
-  color: #90e0ef;
-  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.4);
+  font-size: 2.8rem; /* Taille de la police */
+  font-weight: bold; /* Gras */
+  margin-bottom: 40px; /* Marge en bas */
+  color: #90e0ef; /* Couleur du texte */
+  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.4); /* Ombre du texte */
 
   @media (max-width: 768px) {
-    font-size: 2.5rem;
+    font-size: 2.2rem; /* Taille de police réduite pour les petits écrans */
   }
 `;
 
 // Carte d'article
-const ArticleCard = styled(motion.div)`
-  background: rgba(10, 34, 64, 0.7);
-  border-radius: 1px;
-  overflow: hidden;
-  box-shadow: 12px 12px 10px 
+const ArticleCard = styled.div`
+  background: rgba(10, 34, 64, 0.7); /* Fond semi-transparent */
+  border-radius: 1px; /* Bordure arrondie */
+  overflow: hidden; /* Cache le contenu débordant */
+  box-shadow: 12px 12px 10px rgba(0, 0, 0, 0.4); /* Ombre */
   width: 100%;
-  max-width: 900px;
-  margin: 20px 0;
-  transition: transform 0.3s ease;
+  max-width: 900px; /* Largeur maximale */
+  margin: 20px 0; /* Marge externe */
+  transition: transform 0.3s ease; /* Animation au survol */
 
   &:hover {
-    transform: translateY(-10px);
-    box-shadow: 3px 3px 2px rgba(144, 224, 239);
+    transform: translateY(-10px); /* Effet de levée au survol */
+    box-shadow: 3px 3px 3px rgba(144, 224, 239, 0.6); /* Ombre modifiée au survol */
   }
 `;
 
-// Media (image ou vidéo)
+// Conteneur pour les médias (image ou vidéo)
 const MediaWrapper = styled.div`
   width: 100%;
-  height: 500px;
-  overflow: hidden;
+  height: 500px; /* Hauteur fixe */
+  overflow: hidden; /* Cache le contenu débordant */
 
   img {
     width: 100%;
     height: 100%;
-    object-fit: cover;
+    object-fit: cover; /* Ajuste l'image pour couvrir le conteneur */
   }
-`;
-
-// Contenu d'article
-const ArticleContent = styled.div`
-  padding: 20px;
-  text-align: left;
-  color: rgba(255, 255, 255, 0.85);
-`;
-
-// Titre d'article
-const ArticleTitle = styled.h3`
-  font-size: 1.8rem;
-  font-weight: bold;
-  margin-bottom: 15px;
-  color: #90e0ef;
 
   @media (max-width: 768px) {
-    font-size: 1.5rem;
-    text-align: left;
+    height: 300px; /* Hauteur réduite pour les petits écrans */
   }
 `;
 
-// Description d'article
+// Contenu de l'article
+const ArticleContent = styled.div`
+  padding: 20px; /* Espacement interne */
+  text-align: left; /* Alignement du texte à gauche */
+  color: rgba(255, 255, 255, 0.85); /* Couleur du texte */
+`;
+
+// Titre de l'article
+const ArticleTitle = styled.h3`
+  font-size: 1.8rem; /* Taille de la police */
+  font-weight: bold; /* Gras */
+  margin-bottom: 5px; /* Marge en bas */
+  color: #90e0ef; /* Couleur du texte */
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem; /* Taille de police réduite pour les petits écrans */
+    text-align: left; /* Alignement du texte à gauche */
+  }
+`;
+
+// Description de l'article
 const ArticleDescription = styled.p`
-  font-size: 1rem;
-  line-height: 1.8;
-  margin-bottom: 20px;
+  font-size: 1rem; /* Taille de la police */
+  font-weight: bold; /* Gras */
+  line-height: 1.8; /* Hauteur de ligne */
+  margin-bottom: 20px; /* Marge en bas */
 
   ul {
-    list-style: none;
-    padding: 0;
+    list-style: none; /* Supprime les puces de la liste */
+    padding: 0; /* Supprime le padding par défaut */
 
     h4 {
-      font-size: 1.2rem;
-      color: #90e0ef;
-      margin-top: 40px;
+      font-size: 1.2rem; /* Taille de la police */
+      color: #90e0ef; /* Couleur du texte */
+      margin-top: 40px; /* Marge en haut */
     }
 
     li {
-      margin-bottom: 10px;
+      margin-bottom: 10px; /* Marge en bas pour chaque élément de la liste */
     }
   }
 
   @media (max-width: 768px) {
-    font-size: 0.95rem;
-    line-height: 1.5rem;
-
-    padding: 2rem;
+    font-size: 0.95rem; /* Taille de police réduite pour les petits écrans */
+    line-height: 1.5rem; /* Hauteur de ligne réduite */
+    padding: 1rem; /* Espacement interne réduit */
   }
 `;
 
 // Bouton "Lire plus"
 const ReadMoreButton = styled.a`
-  font-size: 1rem;
-  font-weight: bold;
-  color: #63b3ed;
-  text-decoration: none;
-  cursor: pointer;
-  transition: color 0.3s ease;
+  font-size: 1rem; /* Taille de la police */
+  font-weight: bold; /* Gras */
+  color: #63b3ed; /* Couleur du texte */
+  text-decoration: none; /* Supprime le soulignement */
+  cursor: pointer; /* Curseur en forme de main */
+  transition: color 0.3s ease; /* Animation de transition */
 
   &:hover {
-    color: #ffffff;
-    text-decoration: underline;
+    color: #ffffff; /* Changement de couleur au survol */
+    text-decoration: underline; /* Soulignement au survol */
   }
 `;
+
+// Style pour le fallback (écran de chargement)
+const FallbackContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Hauteur de la vue */
+  background-color: #022026; /* Fond bleu */
+`;
+
+const FallbackLogo = styled.img`
+  width: 100px; /* Largeur du logo */
+  height: auto; /* Hauteur automatique */
+  animation: pulse 1.5s infinite; /* Animation de pulsation */
+
+  @keyframes pulse {
+    0% {
+      transform: scale(0.8); /* Taille normale */
+    }
+    50% {
+      transform: scale(2.2); /* Légèrement agrandi */
+    }
+    100% {
+      transform: scale(1); /* Retour à la taille normale */
+    }
+  }
+`;
+
+// Composant principal
 const Affairee = () => {
+  // Initialisation de AOS pour les animations
+  useEffect(() => {
+    import("aos").then((AOS) => {
+      AOS.init({
+        duration: 1000, // Durée des animations
+        once: false, // Les animations en boucle
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Affairpart1 />
-      <Affairpart2 />
-      <AffairesContainer>
-        <ContentWrapper>
-          <SectionTitle className="font-serif">
-            L’ventages des contrats en droit des affaires
-          </SectionTitle>
-          <ArticleCard
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
-          >
-            <MediaWrapper>
-              <img src={articleImage} alt="Contrats en affaires" />
-            </MediaWrapper>
-            <ArticleContent>
-              <ArticleTitle>
-                Les contrats : une fondation pour les affaires
-              </ArticleTitle>
-              <ArticleDescription>
-                Les contrats occupent une place centrale dans le fonctionnement
-                et le développement du monde des affaires. Voici pourquoi leur
-                importance ne peut être sous-estimée :
-                <ul>
-                  <h4>Un outil juridique indispensable</h4>
-                  <li>
-                    Définition des droits et obligations : Les contrats
-                    définissent clairement les engagements des parties.
-                  </li>
-                  <li>
-                    Base légale pour résoudre les différends : Ils servent de
-                    preuve en cas de conflits.
-                  </li>
-                  <li>
-                    Respect des normes légales : Garantit la conformité avec les
-                    lois.
-                  </li>
-                </ul>
-                <ul>
-                  <h4>Un outil stratégique pour les entreprises</h4>
-                  <li>
-                    Sécurisation des relations commerciales : Renforce la
-                    confiance entre les parties.
-                  </li>
-                  <li>
-                    Gestion des risques : Permet d’anticiper et limiter les
-                    problèmes potentiels.
-                  </li>
-                  <li>
-                    Flexibilité et adaptabilité : Inclut des clauses ajustables
-                    pour faire face aux changements.
-                  </li>
-                </ul>
-                <ul>
-                  <h4>Des applications variées</h4>
-                  <li>
-                    Droit des sociétés : Contrats d’association, partenariats.
-                  </li>
-                  <li>Droit commercial : Vente, distribution, franchise.</li>
-                  <li>
-                    Propriété intellectuelle : Licence, cession, exploitation.
-                  </li>
-                </ul>
-              </ArticleDescription>
-              <ReadMoreButton href="https://aurelienbamde.com/2020/10/15/les-conditions-de-la-gestion-daffaires/">
-                Lire plus
-              </ReadMoreButton>
-            </ArticleContent>
-          </ArticleCard>
-        </ContentWrapper>
-      </AffairesContainer>
-      <Footer />
+      {/* Suspense pour le chargement différé des composants */}
+      <Suspense
+        fallback={
+          <FallbackContainer>
+            {/* Logo de l'entreprise avec animation */}
+            <FallbackLogo src={tiptamcode} alt="TIPTAMCode" />
+          </FallbackContainer>
+        }
+      >
+        {/* Composants chargés de manière différée */}
+        <Affairfinal />
+        <Affair />
+        {/* Conteneur principal */}
+        <AffairesContainer>
+          <ContentWrapper>
+            {/* Titre de la section avec animation */}
+            <SectionTitle data-aos="fade-down">L’importances</SectionTitle>
+            {/* Carte d'article avec animation */}
+            <ArticleCard data-aos="fade-up">
+              <MediaWrapper>
+                {/* Image de l'article avec chargement différé */}
+                <img
+                  src={articleImage}
+                  alt="Contrats en affaires"
+                  data-aos="zoom-in"
+                  loading="lazy"
+                />
+              </MediaWrapper>
+              <ArticleContent>
+                {/* Titre de l'article avec animation */}
+                <ArticleTitle >
+                  Les contrats : une fondation pour les affaires
+                </ArticleTitle>
+                {/* Description de l'article avec animation */}
+                <ArticleDescription >
+                  Les contrats occupent une place centrale dans le
+                  fonctionnement et le développement du monde des affaires.
+                  Voici pourquoi leur importance ne peut être sous-estimée :
+                  <ul>
+                    <h4 >
+                      Un outil juridique indispensable
+                    </h4>
+                    <li>
+                      Définition des droits et obligations : Les contrats
+                      définissent clairement les engagements des parties.
+                    </li>
+                    <li>
+                      Base légale pour résoudre les différends : Ils servent de
+                      preuve en cas de conflits.
+                    </li>
+                    <li>
+                      Respect des normes légales : Garantit la conformité avec
+                      les lois.
+                    </li>
+                  </ul>
+                  <ul>
+                    <h4 >
+                      Un outil stratégique pour les entreprises
+                    </h4>
+                    <li>
+                      Sécurisation des relations commerciales : Renforce la
+                      confiance entre les parties.
+                    </li>
+                    <li>
+                      Gestion des risques : Permet d’anticiper et limiter les
+                      problèmes potentiels.
+                    </li>
+                    <li>
+                      Flexibilité et adaptabilité : Inclut des clauses
+                      ajustables pour faire face aux changements.
+                    </li>
+                  </ul>
+                  <ul>
+                    <h4 >Des applications variées</h4>
+                    <li>
+                      Droit des sociétés : Contrats d’association, partenariats.
+                    </li>
+                    <li>Droit commercial : Vente, distribution, franchise.</li>
+                    <li>
+                      Propriété intellectuelle : Licence, cession, exploitation.
+                    </li>
+                  </ul>
+                </ArticleDescription>
+                {/* Bouton "Lire plus" avec animation */}
+                <ReadMoreButton
+                  href="https://aurelienbamde.com/2020/10/15/les-conditions-de-la-gestion-daffaires/"
+                  data-aos="fade-down"
+                >
+                  Lire plus
+                </ReadMoreButton>
+              </ArticleContent>
+            </ArticleCard>
+          </ContentWrapper>
+        </AffairesContainer>
+        {/* Pied de page */}
+        <Footer />
+      </Suspense>
     </div>
   );
 };
 
-export default Affairee;
+// Exportation du composant avec memo pour optimiser les rendus
+export default React.memo(Affairee);
--- src/Mouvements/Article/Affairpart2.jsx
+++ src/Mouvements/Article/Affairpart2.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import AOS from "aos"; // Importation de AOS pour les animations
+import "aos/dist/aos.css"; // Importation du CSS de AOS
+import { useEffect } from "react"; // Pour initialiser AOS
 import BardeNavigationpublic from "../Navigatpublic/BardeNavigationPublic";
 
 // Conteneur principal avec image d'arrière-plan fixe
@@ -33,7 +35,7 @@ const ContentContainer = styled.div`
 `;
 
 // Titre principal avec style et animation
-const Title = styled(motion.h1)`
+const Title = styled.h1`
   font-size: 3rem;
   color: #90e0ef;
   margin-bottom: 2rem;
@@ -49,7 +51,7 @@ const Title = styled(motion.h1)`
 `;
 
 // Sous-titre pour contextualiser
-const SubTitle = styled(motion.h2)`
+const SubTitle = styled.h2`
   font-size: 1.5rem;
   color: #90e0ef;
   margin-bottom: 1.5rem;
@@ -61,7 +63,7 @@ const SubTitle = styled(motion.h2)`
   }
 
   @media (max-width: 480px) {
-    font-size: 1.3rem;
+    font-size: 1.1rem;
     margin: 1rem;
   }
 `;
@@ -86,16 +88,17 @@ const ScrollAnimation = styled.div`
     background-color: #4ea8ff;
     border-radius: 10px;
   }
+
   @media (max-width: 480px) {
     font-size: 1rem;
     line-height: 1.6rem;
     text-align: left; /* Alignement du texte à gauche */
-    padding: 2rem;
+    padding: 1rem;
   }
 `;
 
 // Texte descriptif avec animation et espacement
-const Description = styled(motion.p)`
+const Description = styled.p`
   font-size: 1.2rem;
   line-height: 1.8;
   max-width: 800px;
@@ -108,14 +111,14 @@ const Description = styled(motion.p)`
   }
 
   @media (max-width: 480px) {
-    font-size: 1.2rem;
+    font-size: 1rem;
     line-height: 1.4;
-    padding: 1rem;
+    padding: 0.5rem;
   }
 `;
 
 // Bouton interactif
-const ActionButton = styled(motion.a)`
+const ActionButton = styled.a`
   margin-bottom: 1rem;
   padding: 0.5rem 0.8rem;
   font-size: 1rem;
@@ -133,90 +136,76 @@ const ActionButton = styled(motion.a)`
 `;
 
 export default function Affairpart2() {
-  return (
-    <BackgroundContainer>
-      {/* Couche de transparence */}
-      <Overlay />
-
-      {/* Barre de navigation */}
-      <BardeNavigationpublic />
-
-      {/* Contenu principal */}
-      <ContentContainer>
-        {/* Titre principal */}
-        <Title
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1 }}
-        >
-          Expertises
-        </Title>
-
-        {/* Sous-titre */}
-        <SubTitle
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.2 }}
-        >
-          Une expertise indispensable pour sécuriser vos transactions
-          commerciales.
-        </SubTitle>
-
-        {/* Texte descriptif défilant */}
-        <ScrollAnimation>
-          <Description
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.5 }}
-          >
-            Parmi les composantes essentielles du droit des affaires, on
-            retrouve le droit commercial, qui encadre les actes de commerce et
-            le statut des commerçants, ainsi que le droit des sociétés, qui
-            régule la création, la gestion et la dissolution des entités
-            juridiques telles que les SARL, SA ou SAS. Ces disciplines
-            permettent aux entrepreneurs de structurer légalement leurs
-            activités tout en protégeant leurs intérêts financiers.
-          </Description>
-          <Description
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.5, delay: 0.2 }}
-          >
-            En outre, le droit des affaires s’étend au droit fiscal, aux
-            contrats commerciaux, à la propriété intellectuelle, et à la
-            concurrence. Par exemple, il garantit une concurrence loyale entre
-            les entreprises tout en protégeant les inventions et marques
-            déposées. Il joue également un rôle crucial dans les processus de
-            médiation et de règlement des litiges entre les partenaires
-            commerciaux, offrant ainsi des solutions rapides et efficaces.
-          </Description>
-          <Description
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.5, delay: 0.4 }}
-          >
-            Enfin, le droit des affaires n’est pas seulement une protection pour
-            les entreprises, mais aussi un outil stratégique. En anticipant les
-            risques juridiques et en respectant les réglementations, les
-            entrepreneurs peuvent consolider leur réputation, attirer des
-            investisseurs, et garantir la pérennité de leurs projets. Ainsi, le
-            droit des affaires est un levier essentiel pour la croissance et le
-            développement économique durable.
-          </Description>
-        </ScrollAnimation>
-
-        {/* Bouton d'action */}
-        <ActionButton
-          href="https://fr.wikipedia.org/wiki/Droit_des_affaires"
-          target="_blank"
-          rel="noopener noreferrer"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-        >
-          +
-        </ActionButton>
-      </ContentContainer>
-    </BackgroundContainer>
-  );
-}
+  // Initialisation de AOS
+   useEffect(() => {
+     AOS.init({
+       duration: 500, // Durée des animations
+       once: false, // Les animations ne se déclenchent en boucle
+     });
+   }, []);
+ 
+   return (
+     <BackgroundContainer>
+       {/* Couche de transparence */}
+       <Overlay />
+ 
+       {/* Barre de navigation */}
+       <BardeNavigationpublic />
+ 
+       {/* Contenu principal */}
+       <ContentContainer>
+         {/* Titre principal */}
+         <Title data-aos="fade-down">Expertises</Title>
+ 
+         {/* Sous-titre */}
+         <SubTitle data-aos="fade-up" data-aos-delay="200">
+           Une expertise indispensable pour sécuriser vos transactions
+           commerciales.
+         </SubTitle>
+ 
+         {/* Texte descriptif défilant */}
+         <ScrollAnimation>
+           <Description data-aos="fade-up" data-aos-delay="200">
+             Parmi les composantes essentielles du droit des affaires, on
+             retrouve le droit commercial, qui encadre les actes de commerce et
+             le statut des commerçants, ainsi que le droit des sociétés, qui
+             régule la création, la gestion et la dissolution des entités
+             juridiques telles que les SARL, SA ou SAS. Ces disciplines
+             permettent aux entrepreneurs de structurer légalement leurs
+             activités tout en protégeant leurs intérêts financiers.
+           </Description>
+           <Description data-aos="fade-down">
+             En outre, le droit des affaires s’étend au droit fiscal, aux
+             contrats commerciaux, à la propriété intellectuelle, et à la
+             concurrence. Par exemple, il garantit une concurrence loyale entre
+             les entreprises tout en protégeant les inventions et marques
+             déposées. Il joue également un rôle crucial dans les processus de
+             médiation et de règlement des litiges entre les partenaires
+             commerciaux, offrant ainsi des solutions rapides et efficaces.
+           </Description>
+           <Description>
+             Enfin, le droit des affaires n’est pas seulement une protection pour
+             les entreprises, mais aussi un outil stratégique. En anticipant les
+             risques juridiques et en respectant les réglementations, les
+             entrepreneurs peuvent consolider leur réputation, attirer des
+             investisseurs, et garantir la pérennité de leurs projets. Ainsi, le
+             droit des affaires est un levier essentiel pour la croissance et le
+             développement économique durable.
+           </Description>
+         </ScrollAnimation>
+ 
+         {/* Bouton d'action */}
+         <ActionButton
+           href="https://fr.wikipedia.org/wiki/Droit_des_affaires"
+           target="_blank"
+           rel="noopener noreferrer"
+           data-aos="fade-up"
+           data-aos-delay="500"
+         >
+           Voir +
+         </ActionButton>
+       </ContentContainer>
+     </BackgroundContainer>
+   );
+ }
+ 

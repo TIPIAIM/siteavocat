098986fe69b7770e7cd64ead6c2c71b04f6ad60c
alphaousmanediallo9 <alphaousmaneousmane@gmail.com>
2025-02-16 02:56:40 +0100

    Fix routes for Vercel98

--- src/Mouvements/Accueil/Accueil.jsx
+++ src/Mouvements/Accueil/Accueil.jsx
@@ -22,8 +22,8 @@ export default function Accueil() {
   return (
     <div style={{ fontFamily: "Helvetica55Roman, Arial, sans-serif" }}>
       <SEO
-        title="AOD AVOCATS SCPA : Cabinet d'avocats - Expertise juridique et services personnalisés"
-        description="Notre cabinet d'avocats offre des services juridiques d'excellence grâce à une équipe d'avocats expérimentés. Nous combinons expertise juridique, conseils personnalisés et solutions juridiques adaptées pour répondre à vos besoins. Faites confiance à notre approche professionnelle pour vos dossiers juridiques et gratuitement d'une assistance juridique sur mesure."
+        title="AOD AVOCATS : Cabinet d'avocats - Expertise juridique et services personnalisés"
+        description="Notre cabinet offre des services juridiques d'excellence grâce à une équipe d'avocats expérimentés. Nous combinons expertise juridique, conseils personnalisés et solutions juridiques adaptées pour répondre à vos besoins. Faites confiance à notre approche professionnelle pour vos dossiers juridiques et gratuitement d'une assistance juridique sur mesure."
         keywords="Cabinet d'avocats, Services juridiques, Avocats expérimentés, Expertise juridique, Approche personnalisée, Excellence juridique, Assistance juridique, Conseils juridiques, Dossiers juridiques, Équipe d'avocats, Solutions juridiques adaptées"
       />
 

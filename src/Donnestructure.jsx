420d74f12c3628a23d735ad86d1468e06bc35c62
alphaousmanediallo9 <alphaousmaneousmane@gmail.com>
2025-02-16 04:48:28 +0100

    Fix routes for Vercel96

--- src/Donnestructure.jsx
+++ src/Donnestructure.jsx
@@ -3,7 +3,7 @@ export default function StructuredData() {
     <script type="application/ld+json">
       {JSON.stringify({
         "@context": "https://schema.org",
-        "@type": "Organization",
+        "@type": "Website",
         name: "AOD AVOCATS",
         url: "https://aod-avocats.net",
       
@@ -13,11 +13,16 @@ export default function StructuredData() {
           "https://web.facebook.com/people/AOD-Avocats-SCPA/61569855070493/?rdid=nu1hpcE1e9TKytvQ&share_url=https%3A%2F%2Fweb.facebook.com%2Fshare%2F15poedCHYP%2F%3F_rdc%3D1%26_rdr",
           "https://www.instagram.com/aod.avocats.net/",
         ],
+        "publisher": {
+          "@type": "Organization",
+          "name": "TIPTMCode."
+        },
         contactPoint: {
           "@type": "ContactPoint",
           telephone: "+224 622 253 536",
           contactType: "Service Juridique",
         },
+
       })}
     </script>
   );
